c001fb504a4552379521a3269ada45776628babd
alundberg1 <anthonyjlundberg@gmail.com>
2019-11-30 22:58:53 -0500

    updated deleteUser to be more similar to addUser

--- client/src/components/deleteUser.js
+++ client/src/components/deleteUser.js
@@ -1,40 +1,81 @@
 import React, {Component} from 'react';
-import '../Login.css';
+import './addUser.css';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
 import axios from 'axios';
 
 class deleteUser extends Component {
 
+  constructor() {
+    super();
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {
+      display : false,
+      modalText: ""
+    };
+  }
+
+  handleClose = () => {
+		var display = this.state.display
+			display = false;
+			this.setState({display});
+  }
+  
+  handleSubmit(event) {
+    event.preventDefault();
+
+if (document.getElementById("inputName").value === "") {
+    this.setState({
+      display: true,
+      modalText:"Error: User Not Deleted! Name Cannot be Empty!"
+  });
+  } else {
+
+ axios({
+  method:'get',
+  url:'http://localhost:3000/Users/list'
+})
+.then(res => {
+  console.log(res.data);
+ 
+})
+.catch(function (error) {
+  console.log(error);
+  this.setState({
+    display: true,
+    modalText:"Error: User Could Not Be Deleted!"
+});
+});
+
+}
+
+document.getElementById("inputName").value = "";
+
+}
+
     render() {
         return (
-            <div class="delete-user-component text-center">
-                <form class="text-center" onsubmit="event.preventDefault(); handleSubmit();">
-                    <label>
-                        Name:
-                    <input type="text" v-model="user.name"/>
-                    </label>
-                    <button type="submit">Submit</button>
-                </form>
+            <div class="add-user-component">
+              <Modal show={this.state.display} onHide={this.handleClose}>
+				      <Modal.Header closeButton>
+					      <Modal.Title>Hello</Modal.Title>
+				      </Modal.Header>
+				      <Modal.Body>{this.state.modalText}</Modal.Body>
+				      <Modal.Footer>
+					      <Button variant="secondary" onClick={this.handleClose}>
+						      Close
+					      </Button>
+				      </Modal.Footer>
+			        </Modal>
+              <form class="text-center" name="user" onSubmit={this.handleSubmit}>
+              <h1 class="h3 mb-3">Delete User</h1>
+                  <label for="inputName" class="sr-only">Name</label>
+                  <input type="text" id="inputName" class="form-control" placeholder="Name" />
+                  <button class="btn btn-lg btn-primary btn-block" type="submit">Submit</button>
+              </form>
             </div>
         );
     }
 }
 
-export default {
-  data() {
-    return {
-      user: {
-        name: ''
-      }
-    }
-  },
-
-  methods: {
-    handleSubmit() {
-      // Send data to the server or update your stores and such.
-      axios.get('http://localhost:3000/Users/', {
-        name: this.name,
-      });
-    alert("User created");
-    }
-  }
-}
+export default deleteUser;
