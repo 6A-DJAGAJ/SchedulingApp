c15fec6ca657942c56572019e37de63c988a5eb5
Austin Appel <austinappel@ufl.edu>
2019-12-03 12:27:56 -0500

    removed unused header elements

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -27,7 +27,6 @@ class Header extends React.Component {
                     <Nav className="mr-auto">
                     <Nav.Link href="#dash" className="nav-colors" onClick={() => this.updatePage('dash')}>Dashboard</Nav.Link>
                     <Nav.Link href="#schedule" className="nav-colors" onClick={() => this.updatePage('schedule')}>Schedule</Nav.Link>
-                    <Nav.Link href="#tasks" className="nav-colors">Tasks</Nav.Link>
                     <Nav.Link href="#timeclock" className="nav-colors" onClick={() => this.updatePage('timeclock')}>Timeclock</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
@@ -46,9 +45,7 @@ class Header extends React.Component {
                     <Nav className="mr-auto">
                     <Nav.Link href="#dash" className="nav-colors" onClick={() => this.updatePage('dash')}>Dashboard</Nav.Link>
                     <Nav.Link href="#schedule" className="nav-colors" onClick={() => this.updatePage('schedule')}>Schedule</Nav.Link>
-                    <Nav.Link href="#tasks" className="nav-colors">Tasks</Nav.Link>
                     <Nav.Link href="#timeclock" className="nav-colors" onClick={() => this.updatePage('timeclock')}>Timeclock</Nav.Link>
-                    <Nav.Link href="#timedata" className="nav-colors">Timeclock Data</Nav.Link>
                     <Nav.Link href="#addUsers" className="nav-colors" onClick={() => this.updatePage('addUsers')}>Add User</Nav.Link>
                     <Nav.Link href="#deleteUsers" className="nav-colors" onClick={() => this.updatePage('deleteUsers')}>Delete User</Nav.Link>
                     <Nav.Link href="#editUsers" className="nav-colors" onClick={() => this.updatePage('editUsers')}>Edit User</Nav.Link>
